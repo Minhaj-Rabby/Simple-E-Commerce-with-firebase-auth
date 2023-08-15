999069cb09d7a2f3600472158e4a6d9df9626d2e
Minhaj-Rabby <minhaj12113@gmail.com>
2023-08-15 23:33:23 +0600

    Card Design added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import './Product.css'
 const Product = (props) => {
-    const{img, name,price,seller,quantity}=props.product;
+    const { img, name, price, seller, ratings } = props.product;
 
-  return (
-    <div  className='product'>
-        <img src={img} alt="product" />
-        <h1>Name:{name}</h1>
-    </div>
-  )
+    return (
+        <div className='product'>
+            <img src={img} alt="product" />
+            <div className='product-info'>
+                <h6 className='product-name'>Name:{name}</h6>
+                <p className='price'>Price: ${price}</p>
+                <p className='manufacturer'> Manufacturer: {seller}</p>
+                <p className='rating'> Rating: {ratings} Star</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
+        </div>
+    )
 }
 
 export default Product
