7b2ae1d0b24610332278dea0e8fe3465ec7b001a
Minhaj-Rabby <minhaj12113@gmail.com>
2023-08-16 01:50:23 +0600

    Cart Calculation

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Cart.css'
+const Cart = (props) => {
+    const { cart } = props;
+    let totalPrice=0;
+    let totalShipping=0;
+
+    for(const product of cart){
+        totalPrice= totalPrice+product.price;
+        totalShipping=totalShipping+product.shipping;
+    }
+    const tax=totalPrice*.1;
+    const grandTotal= totalPrice+totalShipping+tax;
+    return (
+        <div className='cart'>
+            <h5>Order Summery</h5>
+            <p>Selected items: {cart.length}</p>
+            <p>Total Price: ${ totalPrice}</p>
+            <p>Total Shipping Charge: ${ totalShipping}</p>
+            <p>Tax: ${ tax}</p>
+            <h6>Grand Total: ${ grandTotal.toFixed(2)}</h6>
+
+        </div>
+    )
+}
+
+export default Cart
